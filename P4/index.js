93769d74f7b2cdf89a2f881da1fdf7c2c7f4636e
liafig <r.figueroa.2017@alumnos.urjc.es>
2020-04-16 11:27:47 +0200

    mejora emision en pruebas

--- P4/index.js
+++ P4/index.js
@@ -2,32 +2,38 @@ console.log("Ejecutando JS...");
 
 //----- Obtener elemento de video monitor y configurarlo
 const monitor = document.getElementById("monitor")
-monitor.width=600;  //-- Tamaño de la pantalla de video
+monitor.width=600;
 monitor.height=300;
 monitor.poster="https://github.com/liafig/2019-2020-CSAAI-Videos/raw/master/color-bars.png";
 
 //----- Obtener elemento de video 1 y configurarlo
 const video1 = document.getElementById("video1")
-video1.width=300;  //-- Tamaño de la pantalla de video
+video1.width=300;
 video1.height=150;
-video1.src="https://github.com/liafig/2019-2020-CSAAI-Videos/raw/master/dua.mp4"
+video1.src="https://github.com/liafig/2019-2020-CSAAI-Videos/raw/master/dua3.mp4"
 
 //----- Obtener elemento de video 2 y configurarlo
 const video2 = document.getElementById("video2")
-video2.width=300;  //-- Tamaño de la pantalla de video
+video2.width=300;
 video2.height=150;
 video2.src="https://github.com/liafig/2019-2020-CSAAI-Videos/raw/master/dua2.mp4"
 
 //----- Obtener elemento de video 3 y configurarlo
 const video3 = document.getElementById("video3")
-video3.width=300;  //-- Tamaño de la pantalla de video
+video3.width=300;
 video3.height=150;
-video3.src="https://github.com/liafig/2019-2020-CSAAI-Videos/raw/master/dua3.mp4"
+video3.src="https://github.com/liafig/2019-2020-CSAAI-Videos/raw/master/dua.mp4"
+
+const prueba = document.getElementById("prueba")
+prueba.width=200;
+prueba.height=125;
+prueba.src="https://github.com/liafig/2019-2020-CSAAI-Videos/raw/master/emision_pruebas.png"
 
 //-- Obtener los botones
 const play1 = document.getElementById("play1")
 const play2 = document.getElementById("play2")
 const play3 = document.getElementById("play3")
+const test = document.getElementById("test")
 const stop = document.getElementById("stop")
 
 init.onclick = () => {
@@ -37,42 +43,68 @@ init.onclick = () => {
   video3.play();
 };
 
-//-- Función de retrollamada del botón de ver
 play1.onclick = () => {
   console.log("Video 1");
   monitor.src=video1.src;
-  monitor.currentTime = video1.currentTime;
+  monitor.currentTime = video1.currentTime+0.35;
   monitor.play();
-  video1.style.border = "2px solid pink";
+  video1.style.border = "2px solid #ec60e3";
   video2.style.border = "0px";
   video3.style.border = "0px";
+  prueba.style.border = "0px";
 };
 
 play2.onclick = () => {
   console.log("Video 2");
   monitor.src=video2.src;
-  monitor.currentTime = video2.currentTime;
+  monitor.currentTime = video2.currentTime+0.35;
   monitor.play();
   video1.style.border = "0px";
-  video2.style.border = "2px solid pink";
+  video2.style.border = "2px solid #ec60e3";
   video3.style.border = "0px";
+  prueba.style.border = "0px";
 };
 
 play3.onclick = () => {
   console.log("Video 3");
   monitor.src=video3.src;
-  monitor.currentTime = video3.currentTime;
+  monitor.currentTime = video3.currentTime+0.35;
   monitor.play();
   video1.style.border = "0px";
   video2.style.border = "0px";
-  video3.style.border = "2px solid pink";
+  video3.style.border = "2px solid #ec60e3";
+  prueba.style.border = "0px";
 };
 
-//-- Funcion de retrollamada del boton de parar
+test.onclick = () => {
+  console.log("Prueba");
+  monitor.src=null;
+  monitor.poster = prueba.src;
+  monitor.play();
+  video1.style.border = "0px";
+  video2.style.border = "0px";
+  video3.style.border = "0px";
+  prueba.style.border = "2px solid #ec60e3";
+}
+
+auto.onclick = () => {
+  console.log("Automático");
+  document.getElementById("play1").disabled=true;
+  document.getElementById("play2").disabled=true;
+  document.getElementById("play3").disabled=true;
+  document.getElementById("test").disabled=true;
+}
+
+manual.onclick = () => {
+  console.log("Manual");
+  document.getElementById("play1").disabled=false;
+  document.getElementById("play2").disabled=false;
+  document.getElementById("play3").disabled=false;
+  document.getElementById("test").disabled=false;
+}
+
 stop.onclick = () => {
   monitor.pause();
-
-  //-- Quitar la fuente de video, para que se muestre la
-  //-- imagen definida en el atributo poster
   monitor.src=null;
+  monitor.poster="https://github.com/liafig/2019-2020-CSAAI-Videos/raw/master/color-bars.png";
 }
